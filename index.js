452c99c871c47254ce2c95e50c1516b57f49c49d
Andres Gardiol <andresgardiol@gmail.com>
2021-01-25 23:35:02 -0300

    Capítulo 1 - parte 1, Entendiendo el problema
    Una breve descripción del problema que resolveremos y un detalle de las partes importantes

---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+    Link al problema de Advent of Code https://adventofcode.com/2020/day/2
+
+    Parte 1 - Entendiendo el problema
+
+
+    - ¿Cuál es el problema?
+
+        El problema a resolver es: saber cuántas contraseñas (de la lista de contraseñas que nos dan) son válidas según las reglas de validez dadas.
+
+    - Datos útiles:
+
+        - Una lista de contraseñas de ejemplo:
+
+        | 1-3 a: abcde
+        | 1-3 b: cdefg
+        | 2-9 c: ccccccccc
+
+        - Las reglas para saber si una contraseña es válida o no:
+
+            "Cada línea proporciona la política de contraseñas y luego la contraseña. La política de contraseñas indica
+            la más baja y el mayor número de veces que debe aparecer una letra determinada para que la contraseña sea válida.
+            Por ejemplo, 1-3 a significa que la contraseña debe contener al menos 1 vez y como máximo 3 veces."
+
+        - Los casos válidos e inválidos de la lista de ejemplo:
+
+            En el ejemplo anterior, 2 contraseñas son válidas. La contraseña del medio, "cdefg", no lo es; no contiene
+            instancias de "b", pero necesita al menos 1. La primera y tercera contraseñas son válidas: contienen una "a" o nueve "c",
+            ambas dentro de los límites de sus respectivas políticas.
+ */
